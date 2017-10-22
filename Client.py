31fe4d1f9f51e7d5c432935ee044c25c6bb6f37c
Mucishow <massilon@live.com>
2017-10-22 17:47:14 -0200

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,111 @@
+import socket
+import sys
+import tokenize
+from optparse import OptionParser
+
+def parseArg():
+    parser = OptionParser()
+
+    parser.add_option("-p", "--port", dest="Port", help="Port Number to Listen Up", metavar="PORT_NUM",type="int")
+    parser.add_option("-u", "--url", dest="url", help="Url to access")
+
+    (options, args) = parser.parse_args()
+
+    if options.Port == None:
+        options.Port = 80
+    elif options.url == None: 
+        raise RuntimeError("Parameter -u problem")
+    return (options, args)
+
+def getIP(d):
+    """
+    This method returns the first IP address string
+    that responds as the given domain name
+    """
+    try:
+        data = socket.gethostbyname(d)
+        ip = repr(data)
+        return ip
+    except Exception:
+        # fail gracefully!
+        return False
+
+def tokenString(url):
+    tokens = url.split('/')
+    a = getIP(tokens[0])
+    if len(tokens) == 1:
+        tokens.append("") 
+    if a:
+        url = tokens[0]
+        tokens[0] = a
+        return tokens,url
+    else:
+        raise RuntimeError("Not an Valid Address")
+
+def msgCreator(tokens,host):
+    msg = "GET /"
+    tokens.pop(0)
+    if len(tokens) > 0:
+        for token in tokens:
+            if token == '':
+                msg = ''.join(msg+'/')
+                continue
+            msg = ''.join(msg+token)
+            msg = ''.join(msg+'/')
+
+    msg = msg[:-1]
+    msg = ''.join(msg+' HTTP/1.1\n')
+    msg = ''.join(msg+'Host: www.'+host+'\n')
+    msg = ''.join(msg+'Connection: close\n')
+    msg = ''.join(msg+'User-agent: Mozilla/5.0\n\n')
+    return msg
+
+def getFile(socketTCP):
+    rec = socketTCP.recv(1024)
+    msg = rec    
+    while rec:
+        #f.write(rec)
+        rec = socketTCP.recv(1024)
+        msg = msg + rec
+    index = msg.find('\n')    
+    response = msg[:index+1]
+    msg = msg[index:]
+    response = response[:-1]
+    response = response.split(' ')
+    if(response[1] == "200") and (response[2] =="OK"):
+        while True:
+            index = msg.find('\n')
+            msg = msg[index+1:]
+            if(index == 0):
+                break
+
+        f = open("saida",'w')
+        f.write(msg)
+        f.close()
+    else:
+        print "Got a " + response[1]        
+
+(options, args) = parseArg()
+
+#tokenize url
+tokens,url = tokenString(options.url)
+
+#Get the port
+PORT = options.Port
+
+socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+HOST = tokens[0].replace("\"","")
+HOST = HOST.replace("'","")
+
+dest = (HOST, PORT)
+
+if url != 'not-site':
+    msg = msgCreator(tokens,url)
+
+socketTCP.connect(dest)
+
+socketTCP.send(msg)
+
+getFile(socketTCP)
+
---
+++ Server.py
@@ -0,0 +1,127 @@
+import socket
+import sys
+import threading
+from os import listdir
+from os.path import isfile, join, isdir, exists, abspath
+from optparse import OptionParser
+import magic as magic
+
+def parseArg():
+    parser = OptionParser()
+
+    parser.add_option("-p", "--port", dest="Port", help="Port Number to Listen Up", metavar="PORT_NUM",type="int")
+    parser.add_option("-d", "--directory", dest="directory", help="Directory to share")
+
+    (options, args) = parser.parse_args()
+
+    if options.Port == None:
+        raise RuntimeError("Parameter -p problem")
+    elif options.directory == None: 
+        raise RuntimeError("Parameter -d problem")
+    elif not(exists(options.directory)):
+        raise RuntimeError("Directory doenst exist")
+    return (options, args)
+
+def sendFile(clientsocket,url):
+    test = url[:-1]
+    if isfile(test):
+        global mime
+        url = test
+        print mime.from_file(url)
+        responseOK(clientsocket,"HTTP/1.1 200 OK\nContent-Type: "+mime.from_file(url)+"\n\n")
+        f = open(url,'r')
+        l = f.read(1024)
+        while l:
+            clientsocket.send(l)
+            l = f.read(1024)
+    elif isdir(test):
+        responseOK(clientsocket,"HTTP/1.1 200 OK\nContent-Type: Directory\n\n")
+        onlyfiles = []
+        for f in listdir(url):
+            arq = [f]
+            if isfile(url+f):
+                onlyfiles = onlyfiles + arq
+            else:
+                arq[0] = arq[0] + '/'
+                onlyfiles = onlyfiles + arq
+        stringFiles = ''
+        for fi in onlyfiles:
+            stringFiles = ''.join(stringFiles+fi+'\n')
+        #print stringFiles
+        clientsocket.send(stringFiles)
+    else:
+        response(clientsocket,"HTTP/1.1 404 Bad Request\n\n")
+def responseOK(clientsocket,string):
+    clientsocket.send(string)
+
+def response(clientsocket,string):
+    clientsocket.send(string)
+    clientsocket.close()
+
+def getItemUrl(item):
+    tokensItem = item.split('/')
+    tokensItem.pop(0)
+    global Directory
+    if tokensItem[0] == '' and len(tokensItem) == 1:
+        return Directory+'/'
+    
+    else:
+        msg = Directory+'/'
+        if len(tokensItem) > 0:
+            for token in tokensItem:
+                if token == '':
+                    msg = ''.join(msg+'/')
+                    continue
+                tokenConverted = token.replace('%20',' ')
+                msg = ''.join(msg+tokenConverted)
+                msg = ''.join(msg+'/')
+
+        return msg  
+
+def stringClean(rec):
+    tokens = rec.replace('\r','')
+    tokens = rec.split('\n')
+    lineInstruction = tokens[0].replace('\r','')
+    
+    lineInstruction = lineInstruction.split(' ')
+    return lineInstruction
+
+def threadWork(clientsocket,address):
+    rec = clientsocket.recv(1024)
+    instru = stringClean(rec)
+    if len(instru) >= 2:
+        if(instru[0] == 'GET'):
+            archive = getItemUrl(instru[1])
+            sendFile(clientsocket,archive)
+            clientsocket.close()
+        else:
+            response(clientsocket,"HTTP/1.1 501 Not Implemented\n\n")
+    else:
+        response(clientsocket,"HTTP/1.1 404 Bad Request\n\n")
+
+
+
+(options, args) = parseArg()
+mime = magic.Magic(mime=True)
+
+#Get the port
+PORT = options.Port
+Directory = abspath(options.directory)
+print Directory
+socketTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+dest = ("127.0.0.1", PORT)
+
+socketTCP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+socketTCP.bind(dest)
+
+socketTCP.listen(5);
+
+while True:
+    (clientsocket, address) = socketTCP.accept()
+    t = threading.Thread(target=threadWork, args=(clientsocket, address),)   
+    t.run()   
+
+if rec == '':
+    raise RuntimeError("socket connection broker")
+
